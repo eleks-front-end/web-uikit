f9262e1fea9513a74b672a482af7bcf9cbcd47f4
Maksym Sokolovsky <maksym.sokolovskyi@eleks.com>
2016-03-02 15:21:35 +0200

    init standalone search component

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,6 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
-var ghPages     = require('gulp-gh-pages');
-
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
-
-/**
- * Build the Jekyll Site
- */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
-
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
-});
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
@@ -43,13 +8,10 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 gulp.task('sass', function () {
     return gulp.src('styles/**/*.scss')
         .pipe(sass({
-            includePaths: ['styles'],
-            onError: browserSync.notify
+            includePaths: ['styles']
         }))
         .pipe(prefix(['last 2 versions'], { cascade: true }))
-        .pipe(gulp.dest('_site/public/styles'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('public/styles'));
+        .pipe(gulp.dest('./public/styles'));
 });
 
 /**
@@ -58,24 +20,4 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch('styles/**/*.scss', ['sass']);
-    gulp.watch([
-        '**/*.html',
-        '_layouts/*.html',
-        '!_site/**/*.html'
-    ], ['jekyll-rebuild']);
-});
-
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
- */
-gulp.task('default', ['browser-sync', 'watch']);
-
-
-/**
- * Deploy task, deploying site to gh-pages
- */
-gulp.task("deploy", ["jekyll-build"], function () {
-    return gulp.src("./_site/**/*")
-        .pipe(ghPages());
 });
---
+++ search.js
@@ -0,0 +1,8 @@
+.DS_Store
+_site
+.sass-cache
+.jekyll-metadata
+node_modules
+.publish
+public/styles
+.idea
